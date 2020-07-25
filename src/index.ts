7d1c9985f783d0d7823c88748339c36b28af3c1d
luiz victor <fs.luizvictor@gmail.com>
2020-07-25 19:15:27 -0300

    configurando typeorm

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import express from 'express'
 
 import routes from './routes'
 
+import './database/connect'
+
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => console.log('Servers is started at http://localhost:3000'))
+app.listen(3333, () => console.log('Servers is started at http://localhost:3333'))
