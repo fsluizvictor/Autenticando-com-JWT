f77173be7f14e55a626f5ee70d70c7c62c126250
luiz victor <fs.luizvictor@gmail.com>
2020-07-25 22:09:00 -0300

    finalizado

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -5,6 +5,10 @@ import User from '../models/User'
 
 class UserController {
 
+    index(req: Request, res: Response) {
+        return res.send({ userID: req.userId })
+    }
+
     async store(req: Request, res: Response) {
         const repository = getRepository(User)
 
